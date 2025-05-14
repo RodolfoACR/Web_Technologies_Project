1cdbdb24c71cff5f4e4bdf971efdd6a5475bd8ec
JoseDanker <jidanker@miuandes.cl>
2025-05-14 04:28:40 -0400

    bootstrap ready

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,5 @@
 # Pin npm packages by running ./bin/importmap
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
